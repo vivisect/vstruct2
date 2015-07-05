8e518d3a7e48ede1d17fc3d5439a12d95f06066e
invisigoth <invisigoth@kenshoto.com>
2015-07-04 22:23:05 -0400

    fix copypasta for setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     description='Vivisect Structure Definition/Parsing Library',
     author='Invisigoth Kenshoto',
     author_email='invisigoth.kenshoto@gmail.com',
-    url='https://github.com/vivisect/synapse',
+    url='https://github.com/vivisect/vstruct',
     license='Apache License 2.0',
 
     packages=find_packages(exclude=['*.tests','*.tests.*']),
